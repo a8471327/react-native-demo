ba1b877c721c86b8d48b698545348ce663a6c7d1
jiaozesheng <zesheng.jiao@baiyjk.com>
2016-12-29 09:58:34 +0800

    立即安装刷新，杀进程回滚问题

--- index.android.js
+++ index.android.js
@@ -1,57 +1,119 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View, Navigator} from 'react-native';
+import { AppRegistry, Text, View, Dimensions, StyleSheet, TouchableOpacity} from 'react-native';
 import CodePush from 'react-native-code-push';
 
-import MyScene from './module/MyScene';
-
 class TestApp extends Component {
-    componentWillMount() {
-        //访问慢,不稳定
-        CodePush.checkForUpdate('y1xAOHGdxanPlJ_Ph_yBHhSf-1TSEyEVBaHEM').then((update)=>{
-            if(update){
-                CodePush.sync({
-                    deploymentKey: 'y1xAOHGdxanPlJ_Ph_yBHhSf-1TSEyEVBaHEM',
-                    updateDialog: {
-                        optionalIgnoreButtonLabel: '稍后',
-                        optionalInstallButtonLabel: '后台更新',
-                        optionalUpdateMessage: '有新版本了，是否更新？',
-                        title: '更新提示'
-                    },
-                    installMode: CodePush.InstallMode.IMMEDIATE
-                });
-            }
-        });
+    constructor() {
+        super();
+        this.state = {
+            restartAllowed: true,
+            syncMessage: "",
+            progress: ""
+        };
     }
+
+    codePushStatusDidChange(syncStatus) {
+        switch (syncStatus) {
+            case CodePush.SyncStatus.CHECKING_FOR_UPDATE:
+                this.setState({ syncMessage: "Checking for update." });
+                break;
+            case CodePush.SyncStatus.DOWNLOADING_PACKAGE:
+                this.setState({ syncMessage: "Downloading package." });
+                break;
+            case CodePush.SyncStatus.AWAITING_USER_ACTION:
+                this.setState({ syncMessage: "Awaiting user action." });
+                break;
+            case CodePush.SyncStatus.INSTALLING_UPDATE:
+                this.setState({ syncMessage: "Installing update." });
+                break;
+            case CodePush.SyncStatus.UP_TO_DATE:
+                this.setState({ syncMessage: "App up to date.", progress: false });
+                break;
+            case CodePush.SyncStatus.UPDATE_IGNORED:
+                this.setState({ syncMessage: "Update cancelled by user.", progress: false });
+                break;
+            case CodePush.SyncStatus.UPDATE_INSTALLED:
+                this.setState({ syncMessage: "Update installed and will be applied on restart.", progress: false });
+                break;
+            case CodePush.SyncStatus.UNKNOWN_ERROR:
+                this.setState({ syncMessage: "An unknown error occurred.", progress: false });
+                break;
+        }
+    }
+
+    codePushDownloadDidProgress(progress) {
+        this.setState({ progress });
+    }
+
+    sync() {
+        CodePush.sync({installMode: CodePush.InstallMode.ON_NEXT_RESUME},
+            this.codePushStatusDidChange.bind(this),
+            this.codePushDownloadDidProgress.bind(this)
+        );
+    }
+
+    syncImmediate(){
+        CodePush.sync(
+            {
+                updateDialog: {
+                    optionalIgnoreButtonLabel: '稍后',
+                    optionalInstallButtonLabel: '后台更新',
+                    optionalUpdateMessage: '有新版本了，是否更新？',
+                    title: '更新提示'
+                },
+                installMode: CodePush.InstallMode.IMMEDIATE
+            },
+            this.codePushStatusDidChange.bind(this),
+            this.codePushDownloadDidProgress.bind(this)
+        );
+    }
+
     render() {
         return (
-            <Navigator
-                initialRoute={{ title: 'My Initial Scene(第二次更新)', index: 0 }}
-                renderScene={(route, navigator) =>
-                    <MyScene
-                        title={route.title}
-                        onForward={() => {
-                            const nextIndex = route.index + 1;
-                            navigator.push({
-                                title: 'Scene ' + nextIndex,
-                                index: nextIndex
-                            });
-                        }}
-                        onBack={() => {
-                            if(route.index > 0){
-                                navigator.pop();
-                            }
-                        }}
-                    >
-                    </MyScene>
-                }
-                configureScene={ (route, routeStack) => {
-                    return Navigator.SceneConfigs.HorizontalSwipeJump
-                }}
-            >
-            </Navigator>
+            <View style={styles.container}>
+                <Text>版本0</Text>
+                <Text style={styles.messages}>{this.state.progress.receivedBytes} of {this.state.progress.totalBytes} bytes received</Text>
+                <Text style={styles.messages}>{this.state.syncMessage || ""}</Text>
+                <TouchableOpacity onPress={this.sync.bind(this)}>
+                    <Text style={styles.syncButton}>Press for background sync</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={this.syncImmediate.bind(this)}>
+                    <Text style={styles.syncButton}>Press for dialog-driven sync</Text>
+                </TouchableOpacity>
+            </View>
         );
     }
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        backgroundColor: "#F5FCFF",
+        paddingTop: 50
+    },
+    image: {
+        margin: 30,
+        width: Dimensions.get("window").width - 100,
+        height: 365 * (Dimensions.get("window").width - 100) / 651,
+    },
+    messages: {
+        marginTop: 30,
+        textAlign: "center",
+    },
+    restartToggleButton: {
+        color: "blue",
+        fontSize: 17
+    },
+    syncButton: {
+        color: "green",
+        fontSize: 17
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: "center",
+        margin: 20
+    },
+});
 
-AppRegistry.registerComponent('TestApp', () => TestApp);
+AppRegistry.registerComponent('TestApp', () => CodePush({ checkFrequency: CodePush.CheckFrequency.MANUAL })(TestApp));
